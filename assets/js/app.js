2343712b193a6c3bf1f2f5475ae5160db358d8b2
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2018-10-05 10:46:04 -0500

    UPDATE: responsive nav bar, need to fix some errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,13 @@
-//Grafica circular
-$(document).ready(function () {
-    $("#gra-circular").circliful({
-        animation: 1,
-        animationStep: 5,
-        foregroundBorderWidth: 7,
-        backgroundBorderWidth: 7,
-        textSize: 28,
-        textStyle: 'font-size: 12px;',
-        textColor: '#666',
-        multiPercentage: 1,
-        percentages: [{
-                'percent': 80,
-                'color': '#3180B8',
-                'title': 'Ingreso de estudiantes'
-            },
-            {
-                'percent': 75,
-                'color': '#4ADBEA',
-                'title': 'Ingreso Directores'
-            },
-            {
-                'percent': 70,
-                'color': '#49EBA8',
-                'title': 'Ingreso de Jurados'
-            },
-            {
-                'percent': 50,
-                'color': '#FFCA35',
-                'title': 'Total visitas'
-            }
-        ],
-        multiPercentageLegend: 1,
-        replacePercentageByText: '',
-        backgroundColor: '#eee'
-
-    });
-});
+//Navbar
+function topNav(){
+    var display = document.getElementById("Dropdown");
+    if(display.className === "ul-lista"){
+        display.className += " responsive";
+    }
+    else{
+        display.className = "ul-lista";
+    }
+}
 
 //Circle library
 function shuffle(o) { //v1.0
@@ -234,3 +206,4 @@ signupBtn.addEventListener('click', (e) => {
 		}
 	});
 });
+
