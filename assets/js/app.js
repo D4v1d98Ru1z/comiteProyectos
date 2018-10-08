b61d97f64c796e566e1c6fadec283066820caa32
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-10-08 16:48:06 -0500

    ADD: carousel done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
 //Navbar
-function topNav(){
+function topNav() {
     var display = document.getElementById("Dropdown");
-    if(display.className === "ul-lista"){
+    if (display.className === "ul-lista") {
         display.className += " responsive";
-    }
-    else{
+    } else {
         display.className = "ul-lista";
     }
 }
@@ -25,48 +24,48 @@ var colors = [
 
 // For each insight made one
 var child = document.getElementById('circles-' + 1),
-            percentage = 80;
-        document.getElementById("percent").innerHTML = "Estudiantes ingresados " + percentage + "%";
-        circles.push(Circles.create({
-            id: child.id,
-            value: percentage,
-            radius: 80,
-            width: 10,
-            colors: colors[1 - 1]
-        }));
-        //2
-        var child = document.getElementById('circles-' + 2),
-            percentage = 75;
-        document.getElementById("percent1").innerHTML = "Directores ingresados " + percentage + "%";
-        circles.push(Circles.create({
-            id: child.id,
-            value: percentage,
-            radius: 80,
-            width: 10,
-            colors: colors[2 - 1]
-        }));
-        //3
-        var child = document.getElementById('circles-' + 3),
-            percentage = 70;
-        document.getElementById("percent2").innerHTML = "Jurados ingresados " + percentage + "%";
-        circles.push(Circles.create({
-            id: child.id,
-            value: percentage,
-            radius: 80,
-            width: 10,
-            colors: colors[3 - 1]
-        }));
-        //4
-        var child = document.getElementById('circles-' + 4),
-            percentage = 50;
-        document.getElementById("percent3").innerHTML = "Total de visitas " + percentage + "%";
-        circles.push(Circles.create({
-            id: child.id,
-            value: percentage,
-            radius: 80,
-            width: 10,
-            colors: colors[4 - 1]
-        }));
+    percentage = 80;
+document.getElementById("percent").innerHTML = "Estudiantes ingresados " + percentage + "%";
+circles.push(Circles.create({
+    id: child.id,
+    value: percentage,
+    radius: 80,
+    width: 10,
+    colors: colors[1 - 1]
+}));
+//2
+var child = document.getElementById('circles-' + 2),
+    percentage = 75;
+document.getElementById("percent1").innerHTML = "Directores ingresados " + percentage + "%";
+circles.push(Circles.create({
+    id: child.id,
+    value: percentage,
+    radius: 80,
+    width: 10,
+    colors: colors[2 - 1]
+}));
+//3
+var child = document.getElementById('circles-' + 3),
+    percentage = 70;
+document.getElementById("percent2").innerHTML = "Jurados ingresados " + percentage + "%";
+circles.push(Circles.create({
+    id: child.id,
+    value: percentage,
+    radius: 80,
+    width: 10,
+    colors: colors[3 - 1]
+}));
+//4
+var child = document.getElementById('circles-' + 4),
+    percentage = 50;
+document.getElementById("percent3").innerHTML = "Total de visitas " + percentage + "%";
+circles.push(Circles.create({
+    id: child.id,
+    value: percentage,
+    radius: 80,
+    width: 10,
+    colors: colors[4 - 1]
+}));
 
 $(window).resize(function () {
     if ($(window).width() <= 404) {
@@ -184,26 +183,102 @@ const loginBtn = document.getElementById('login');
 const signupBtn = document.getElementById('signup');
 
 loginBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode.parentNode;
-	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			signupBtn.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
+    let parent = e.target.parentNode.parentNode;
+    Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+        if (element !== "slide-up") {
+            parent.classList.add('slide-up')
+        } else {
+            signupBtn.parentNode.classList.add('slide-up')
+            parent.classList.remove('slide-up')
+        }
+    });
 });
 
 signupBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode;
-	Array.from(e.target.parentNode.classList).find((element) => {
-		if(element !== "slide-up") {
-			parent.classList.add('slide-up')
-		}else{
-			loginBtn.parentNode.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		}
-	});
+    let parent = e.target.parentNode;
+    Array.from(e.target.parentNode.classList).find((element) => {
+        if (element !== "slide-up") {
+            parent.classList.add('slide-up')
+        } else {
+            loginBtn.parentNode.parentNode.classList.add('slide-up')
+            parent.classList.remove('slide-up')
+        }
+    });
 });
 
+//Carousel
+/*
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'flipInX',
+        items:1,
+        margin:30,
+        stagePadding:30,
+        smartSpeed:450
+    });
+  });
+  */
+carousel();
+
+function carousel() {
+    $('#my-img').css({
+        background: "linear-gradient(60deg, rgba(255, 255, 255, 0.99) 30%, transparent 75%), url(../assets/img/bg-2.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+    });
+
+    var bar = document.getElementById("bar");
+    var width = 1;
+    var id = setInterval(frame, 150);
+    var status = 1;
+
+    function frame() {
+        if (width == 100) {
+
+            switch (status) {
+                case 1:
+                    {
+                        status++;
+                        $('#my-img').fadeIn();
+                        $('#my-img').css({
+                            background: "linear-gradient(60deg, rgba(255, 255, 255, 0.99) 30%, transparent 75%), url(../assets/img/bg-2.jpg)",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center"
+                        });
+                        break;
+                    }
+                case 2:
+                    {
+                        status++;
+                    
+                        $('#my-img').fadeIn();
+                        $('#my-img').css({
+                            background: "linear-gradient(60deg, rgba(255, 255, 255, 0.99) 30%, transparent 75%), url(../assets/img/1.jpg)",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center"
+                        });
+                        break;
+                    }
+                case 3:
+                    {
+           
+                        status = 1;
+                        $('#my-img').fadeIn();
+                        $('#my-img').css({
+                            background: "linear-gradient(60deg, rgba(255, 255, 255, 0.99) 30%, transparent 75%), url(../assets/img/bg-1.jpg)",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center"
+                        });
+                        break;
+                    }
+            }
+            clearInterval(id);
+            id = setInterval(frame, 150);
+            width = 0;
+        } else {
+            width++;
+            bar.style.width = width + '%';
+        }
+    }
+}
